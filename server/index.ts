4504204d5b3990d3f8fb20418888ecb5b7b54782
Dane Miller <khadane.miller@gmail.com>
2021-10-08 20:29:47 -0400

    chore: clean up entrypoint file and add controllers and routes

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,29 @@
 import { App } from "@tinyhttp/app";
 import morgan from "morgan";
 
-const app = new App();
+const app = new App({
+  onError: (error, _, response) => {
+    response.status(500).json({
+      status: 500,
+      title: "There was an error processing your request.",
+      detail: `Error ${error}`,
+    });
+  },
+  noMatchHandler: (request, response) => {
+    response.status(404).json({
+      status: 404,
+      title: "The path you requested could not be found on the server.",
+      detail: `The path: ${request.path} could not be located. Is this the correct path?`,
+    });
+  },
+});
 
 app.use(morgan("dev"));
 
 app.get("/", (_, response) => {
-  response.send("henlo from tinyhttp");
+  response.send("Welcome to the Conduit API");
 });
 
-app.listen(4000, () => console.info("Started server on http://localhost:4000"));
+app.listen(4000, () =>
+  console.info("Started server on: http://localhost:4000")
+);
