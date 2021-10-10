aa3fd38b688f3d9e53cba6f830f650bb583e9046
Dane Miller <khadane.miller@gmail.com>
2021-10-10 17:12:22 -0400

    refactor: remove follow functionality

--- server/controllers/profile.controller.ts
+++ server/controllers/profile.controller.ts
@@ -32,38 +32,7 @@ export = {
         });
       }
 
-      return response.status(200).json(user);
-    } catch (error) {
-      return next(createServerError(error));
-    }
-  },
-  followProfile: async (
-    request: Request<{ profile: Omit<Profile, 'id' | 'userId'> }>,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const {
-        profile: { username },
-      } = request.params;
-      const user = await db.profile.findUnique({
-        where: {
-          username,
-        },
-        select: {
-          username: true,
-          bio: true,
-          image: true,
-        },
-      });
-
-      if (!user) {
-        return next({
-          status: 404,
-          title: Message.NOTFOUND,
-          description: `Profile not found for user ${user.username}`,
-        });
-      }
+      return response.status(200).json({ profile: { ...user } });
     } catch (error) {
       return next(createServerError(error));
     }
